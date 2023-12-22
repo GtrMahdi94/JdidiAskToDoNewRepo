922c7ed40e8bb98429b83f694716afb3392dc50d
Gatri Mahdi <gatrimahdi5@gmail.com>
2023-12-22 15:22:26 +0100

    sexe add

---
+++ Sexe.js
@@ -0,0 +1,10 @@
+function  YourSexe (YourGendre){
+    if ((YourGendre==="Male")||(YourGendre==="Female")){
+       return ("Your Gendre :"+" "+YourGendre+" !")
+   
+    }
+   
+   else {
+       return "choose Male or Female"
+   }
+   }
